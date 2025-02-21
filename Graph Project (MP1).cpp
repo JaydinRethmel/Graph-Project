51d141bc74433b060bbbedf70885dd428ca1a808
jaydin rethmel <143096716+JaydinRethmel@users.noreply.github.com>
2025-02-21 10:36:55 -0500

    Project Created
    This is the first commitment to this project. While a GitHub repository is not a requirement for this assignment, I can now practice my version control and learn more about how GitHub functions.

---
+++ Graph Project (MP1).cpp
@@ -0,0 +1,12 @@
+// Title - Graph Project (MP1)
+// Author - Jaydin Rethmel
+// Description - This project is a assignment for course ECCS2681 - Data Structures and Algorithms 2. The goal for this project is to write code that
+//               Stores graph information in bot ha adjacency matrix and adjacency list. This is a relatively simple project that will help me understand
+//               how to work with graphs in C++, and improve my skills with data structures and algorithms.
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
